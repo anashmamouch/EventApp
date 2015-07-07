8aa446a1281ab0ad5c07f05eb66f81bdce198ca8
Anas Hmamouch <anashmamouch90@gmail.com>
2015-07-07 10:31:47 +0000

    this is an octocat :octocat:

--- app/src/main/java/com/benzino/eventapp/MainActivity.java
+++ app/src/main/java/com/benzino/eventapp/MainActivity.java
@@ -3,16 +3,20 @@ package com.benzino.eventapp;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 
     @Override
